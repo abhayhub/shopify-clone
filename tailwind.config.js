ea7930600521558755ad077b6c7896dc7d20c4ba
Abhay tiwari <abhayskp7948>
2024-02-16 22:17:24 +0530

    shopify clone done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontSize:{
+      'xl':'1.25rem',
+    },
+    colors:{
+      'fav': '#043264d2',
+      'white':'#fff',
+    },
+    extend: {
+      margin: {
+        '5px': '50px',
+      }
+    },
+  },
+  plugins: [],
+}
